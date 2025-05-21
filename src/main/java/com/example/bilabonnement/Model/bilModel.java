876dbb98a53151cb10d177e976888ad9546eb364
Tarik2300 <tana0001@stud.kea.dk>
2025-05-21 13:41:20 +0200

    Bil metoder og html tests

--- src/main/java/com/example/bilabonnement/Model/bilModel.java
+++ src/main/java/com/example/bilabonnement/Model/bilModel.java
@@ -1,4 +1,124 @@
 package com.example.bilabonnement.Model;
 
 public class bilModel {
+    private int bilId;
+    private String vognNummer;
+    private String stelNummer;
+    private String regNr;
+    private String model;
+    private String maerke;
+    private double co2;
+    private boolean tilgaengelig;
+    private double staalpris;
+    private int udstyrsniveauId;
+
+    // --- Getters og setters ---
+
+    public int getBilId() {
+        return bilId;
+    }
+
+    public void setBilId(int bilId) {
+        this.bilId = bilId;
+    }
+
+    public String getVognNummer() {
+        return vognNummer;
+    }
+
+    public void setVognNummer(String vognNummer) {
+        this.vognNummer = vognNummer;
+    }
+
+    public String getStelNummer() {
+        return stelNummer;
+    }
+
+    public void setStelNummer(String stelNummer) {
+        this.stelNummer = stelNummer;
+    }
+
+    public String getRegNr() {
+        return regNr;
+    }
+
+    public void setRegNr(String regNr) {
+        this.regNr = regNr;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMaerke() {
+        return maerke;
+    }
+
+    public void setMaerke(String maerke) {
+        this.maerke = maerke;
+    }
+
+    public double getCo2() {
+        return co2;
+    }
+
+    public void setCo2(double co2) {
+        this.co2 = co2;
+    }
+
+    public boolean isTilgaengelig() {
+        return tilgaengelig;
+    }
+
+    public void setTilgaengelig(boolean tilgaengelig) {
+        this.tilgaengelig = tilgaengelig;
+    }
+
+    public double getStaalpris() {
+        return staalpris;
+    }
+
+    public void setStaalpris(double staalpris) {
+        this.staalpris = staalpris;
+    }
+
+    public int getUdstyrsniveauId() {
+        return udstyrsniveauId;
+    }
+
+    public void setUdstyrsniveauId(int udstyrsniveauId) {
+        this.udstyrsniveauId = udstyrsniveauId;
+    }
+
+    // --- Optional: toString() til debug ---
+    @Override
+    public String toString() {
+        return "bilModel{" +
+                "bilId=" + bilId +
+                ", vognNummer='" + vognNummer + '\'' +
+                ", stelNummer='" + stelNummer + '\'' +
+                ", regNr='" + regNr + '\'' +
+                ", model='" + model + '\'' +
+                ", maerke='" + maerke + '\'' +
+                ", co2=" + co2 +
+                ", tilgaengelig=" + tilgaengelig +
+                ", staalpris=" + staalpris +
+                ", udstyrsniveauId=" + udstyrsniveauId +
+                '}';
+    }
+
+    private String udstyrsniveauNavn;
+
+    public String getUdstyrsniveauNavn() {
+        return udstyrsniveauNavn;
+    }
+
+    public void setUdstyrsniveauNavn(String udstyrsniveauNavn) {
+        this.udstyrsniveauNavn = udstyrsniveauNavn;
+    }
+
 }
--- src/main/java/com/example/bilabonnement/Repository/bilRepo.java
+++ src/main/java/com/example/bilabonnement/Repository/bilRepo.java
@@ -1,4 +1,88 @@
 package com.example.bilabonnement.Repository;
 
+import com.example.bilabonnement.Model.bilModel;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import com.example.bilabonnement.Model.udstyrsniveau;
+
+import java.util.List;
+
+@Repository
 public class bilRepo {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public bilRepo(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // --- Hent alle biler ---
+    public List<bilModel> hentAlleBiler() {
+        String sql = """
+        SELECT b.*, u.navn AS udstyrsniveauNavn
+        FROM Bil b
+        JOIN Udstyrsniveau u ON b.udstyrsniveau_ID = u.udstyrsniveau_ID
+        """;
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(bilModel.class));
+    }
+
+
+    // --- Opret ny bil ---
+    public void opretBil(bilModel bil) {
+        String sql = "INSERT INTO Bil (vognNummer, stelNummer, regNr, model, maerke, co2, tilgaengelig, staalpris, udstyrsniveau_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql,
+                bil.getVognNummer(),
+                bil.getStelNummer(),
+                bil.getRegNr(),
+                bil.getModel(),
+                bil.getMaerke(),
+                bil.getCo2(),
+                bil.isTilgaengelig(),
+                bil.getStaalpris(),
+                bil.getUdstyrsniveauId()
+        );
+    }
+
+    // --- Find én bil via ID ---
+    public bilModel findById(int id) {
+        String sql = "SELECT * FROM Bil WHERE bil_ID = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{id},
+                new BeanPropertyRowMapper<>(bilModel.class));
+    }
+
+    // --- Opdater bil ---
+    public void opdaterBil(bilModel bil) {
+        String sql = "UPDATE Bil SET vognNummer = ?, stelNummer = ?, regNr = ?, model = ?, maerke = ?, co2 = ?, tilgaengelig = ?, staalpris = ?, udstyrsniveau_ID = ? WHERE bil_ID = ?";
+        jdbcTemplate.update(sql,
+                bil.getVognNummer(),
+                bil.getStelNummer(),
+                bil.getRegNr(),
+                bil.getModel(),
+                bil.getMaerke(),
+                bil.getCo2(),
+                bil.isTilgaengelig(),
+                bil.getStaalpris(),
+                bil.getUdstyrsniveauId(),
+                bil.getBilId()
+        );
+    }
+
+    // --- Slet bil ---
+    public void sletBil(int id) {
+        String sql = "DELETE FROM Bil WHERE bil_ID = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    public List<udstyrsniveau> hentAlleUdstyrsniveauer() {
+        String sql = "SELECT * FROM Udstyrsniveau";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(udstyrsniveau.class));
+    }
+
+    public List<bilModel> hentLedigeBiler() {
+        String sql = "SELECT * FROM Bil WHERE tilgaengelig = true";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(bilModel.class));
+    }
+
 }
+
