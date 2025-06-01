cdf2b00e1ebfca4c06a5f64fb70ce7bd7e4f40ac
alijr8 <ahmedali.alh8@gmail.com>
2025-06-01 21:26:52 +0200

    kommentarer tilføjet til alt der er relateret til kunde

--- src/main/java/com/example/bilabonnement/Service/kundeService.java
+++ src/main/java/com/example/bilabonnement/Service/kundeService.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+// Denne klasse er service-laget for kunder og fungerer som bindeled mellem controller og repository
+// Service-laget indeholder forretningslogik og sikrer, at controlleren ikke behøver at bekymre sig om databasen direkte
 @Service
 public class kundeService {
 
+    // Her laver vi en attribut til kundeRepo, som vi bruger til at kalde metoderne, der arbejder direkte med databasen
     private final kundeRepo kundeRepo;
 
+    // Parametiseret constructor – Spring sørger for at "indsprøjte" kundeRepo, så vi kan bruge det her
+    // Her bliver alle metoder genbrugt fra repo som har CRUD-operationer
     public kundeService(kundeRepo kundeRepo) {
         this.kundeRepo = kundeRepo;
     }
